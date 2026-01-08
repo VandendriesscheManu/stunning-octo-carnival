e51f4758d0657427865e8df07179484927946d8f
VandendriesscheManu <manu.vandendriessche@student.howest.be>
2026-01-08 15:50:49 +0100

    Update API_BASE_URL to the correct endpoint in app.py

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 API_BASE_URL = os.getenv(
     "API_BASE_URL",
-    "https://ongoing-sql-nerve-learners.trycloudflare.com ",
+    "https://chip-drilling-sky-conditioning.trycloudflare.com",
 )
 
 # Lees API key uit env (Streamlit Cloud Secrets)
